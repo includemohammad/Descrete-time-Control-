c021e7bf9bcd6d3a9f8144552905d46b910b5a18
includemohammad <127312425+includemohammad@users.noreply.github.com>
2025-04-20 20:52:44 +0330

    Update DBZ_Control_DT_LQR.m

--- DBZ_Control_DT_LQR.m
+++ DBZ_Control_DT_LQR.m
@@ -1,3 +1,16 @@
+% =========================================================================
+%  Self-Organizing Map (SOM) Neural Network
+%  Author      : Mohammad Dehbozorgi (MO.DBZ)
+%  Created on  : 1404/01/31 (Persian Calendar)
+%  Updated on  : 2025/04/20
+%  Description : Competitive DT LQR with idare
+% =========================================================================
+%  License:
+%  This code is provided as-is without any warranty. 
+%  You may use, modify, and distribute it for educational 
+%  and research purposes with proper credit to the author.
+% =========================================================================
+
 clc;
 clear all;
 close all ;
